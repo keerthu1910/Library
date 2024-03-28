c6d75e761063ae8f074c7b107451ef74bc4f72e5
keerthu1910 <keerthiselva1910@gmail.com>
2024-03-28 20:43:23 +0530

    Feat(server) : Include backend config file

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -3,5 +3,21 @@ import dotenv from 'dotenv';
 if(dotenv) dotenv.config();
 
 export const config = {
-    port:process.env.PORT || 3001
+    port:process.env.PORT || 3001,
+    admin:{
+        admin_id:process.env.ADMIN_ID,
+        admin_password:process.env.ADMIN_PASSWORD,
+        admin_email:process.env.ADMIN_EMAIL,
+        admin_mailpassword:process.env.ADMIN_EMAIL_PASSWORD
+    },
+    database:{
+        host:process.env.HOST,
+        port:parseInt(process.env.DATABASE_PORT || '3306'),
+        username:process.env.DATABASE_USERNAME,
+        password:process.env.DATABASE_PASSWORD,
+        db:process.env.DATABASE_NAME
+    },
+    jwt:{
+        secret:process.env.JWT_SECRET
+    }
 }
