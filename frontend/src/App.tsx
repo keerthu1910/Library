1bd38eba2526e7859a7f1ff39b76a1ffce338d98
keerthu1910 <keerthiselva1910@gmail.com>
2024-03-28 20:17:00 +0530

    Include App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import { Login } from './Components/Login/Login';
+import { Newuser } from './Components/Newuser';
+import { Profile } from './Components/Profile/Profile';
+import axios from 'axios';
+import {Routes,Route} from 'react-router-dom';
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Forgotpassword, Mailresetpassword, ResetPassword } from './Components';
+axios.defaults.baseURL = import.meta.env.VITE_APP_END_POINT;
+axios.defaults.withCredentials = true;
 
 function App() {
 
   return (
     <div>
-      <h1>Library</h1>
+      <ToastContainer />
+      <Routes>
+        <Route path='/' element={<Login />}/>
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/newuser' element={<Newuser />} />
+        <Route path='/resetpassword' element={<ResetPassword />} />
+        <Route path='/forgotpassword' element={<Forgotpassword />} />
+        <Route path='/resetmailpassword' element={<Mailresetpassword />} />
+      </Routes>
+    
     </div>
   )
 }
